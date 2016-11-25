eb741c97e5d82703e6c259a83a071388710a81dd
imac <imac@viatick.com>
2016-11-25 18:17:37 +0800

    Useless IBM documentation

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -67,7 +67,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
-func (t*SimpleChaincode) write(stub shim.ChaincodeStubInterface, args[] string) ([]byte, error) {
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args[] string) ([]byte, error) {
 	var key, value string
 	var err error
 	fmt.Println("running write()")
